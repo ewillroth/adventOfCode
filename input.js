76dd777fd55e5af1bfa106d99779a094b67ee5a0
Eric Willroth <ewillroth@gmail.com>
2022-11-30 22:13:33 -0700

    2022 day 1

--- input.js
+++ input.js
@@ -24,24 +24,3 @@ axios
   .catch((error) => {
     console.log(error.response.data, 'error');
   });
-
-// axios
-//   .get(
-//     `https://adventofcode.com/${process.env.YEAR}/day/${process.env.DAY}/input`,
-//     (response) => {
-//       let data = '';
-
-//       // A chunk of data has been received.
-//       response.on('data', (chunk) => {
-//         data += chunk;
-//       });
-
-//       // The whole response has been received. Print out the result.
-//       response.on('end', () => {
-//         console.log(JSON.parse(data).explanation);
-//       });
-//     }
-//   )
-//   .on('error', (err) => {
-//     console.log('Error: ' + err.message);
-//   });
---
+++ notes.md
@@ -0,0 +1,1 @@
+find path with djikstra - 2021 day 15
