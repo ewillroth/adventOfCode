d6d321d325fa54956b623f86c96ee53be294a369
Eric Willroth <ewillroth@gmail.com>
2022-12-14 00:13:48 -0700

    2022 day 14 progress

--- 2022/13.js
+++ 2022/13.js
@@ -1,4 +1,152 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('./2022/13_input.txt', 'utf-8');
-const rows = input.split('\n');
+const input = fs.readFileSync('./2022/13_input.txt', 'utf-8').trim();
+
+const testInput = fs.readFileSync('./2022/13_testInput.txt', 'utf-8').trim();
+
+const getPairs = (input) => {
+  const pairs = input.split('\n\n');
+  return pairs.map((pair) => {
+    const [packet1, packet2] = pair.split('\n');
+    return { packet1: JSON.parse(packet1), packet2: JSON.parse(packet2) };
+  });
+};
+
+const areValuesInvalid = (val1, val2, innerI) => {
+  if (val1 !== undefined && val2 === undefined) {
+    return true;
+  }
+  if (val1 === undefined && val2 !== undefined) {
+    return false;
+  }
+  if (typeof val1 === 'number' && typeof val2 === 'number') {
+    if (val1 > val2) return true;
+    if (val1 < val2) return false;
+    return undefined;
+  }
+
+  if (typeof val1 === 'number' && typeof val2 === 'object') {
+    val1 = [val1];
+  }
+  if (typeof val1 === 'object' && typeof val2 === 'number') {
+    val2 = [val2];
+  }
+  if (typeof val1 === 'object' && typeof val2 === 'object') {
+    const biggerArray = val1.length > val2.length ? val1.length : val2.length;
+    // if right array is bigger - compare all the values inside the arrays
+    for (let i = 0; i < biggerArray; i++) {
+      const result = areValuesInvalid(val1[i], val2[i], i);
+      if (result === false) {
+        return false;
+      } else if (result === true) {
+        return true;
+      } else {
+        continue;
+      }
+    }
+  }
+};
+
+const comparePairs = (pairsObject, index) => {
+  let valid;
+  const { packet1, packet2 } = pairsObject;
+  const biggerArray =
+    packet1.length > packet2.length ? packet1.length : packet2.length;
+  for (let i = 0; i < biggerArray; i++) {
+    const invalid = areValuesInvalid(packet1[i], packet2[i]);
+    if (invalid === true) {
+      valid = false;
+      break;
+    } else if (invalid === false) {
+      valid = true;
+      break;
+    } else {
+      continue;
+    }
+  }
+  return valid;
+};
+
+const part1 = () => {
+  const validPairs = [];
+  const pairs = getPairs(input);
+  pairs.forEach((pair, index) => {
+    const valid = comparePairs(pair, index);
+    if (valid) validPairs.push(index + 1);
+  });
+  console.log(validPairs.reduce((a, b) => a + b, 0));
+};
+
+part1();
+
+// https://stackoverflow.com/questions/9960908/permutations-in-javascript
+const permutator = (inputArr) => {
+  let count = 0;
+  console.time('omg');
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      count++;
+      console.log(`permutation ${count}`);
+
+      // check if all pairs are valid using the method from part 1
+      const allValid = checkIfAllPairsAreValid(m);
+      if (allValid) {
+        console.log('answer is true');
+        console.log(
+          'index for [[2]]',
+          Object.values(pairs).findIndex(
+            (pair) => pair.packet1 === '[[2]]' || pair.packet1 === '[[2]]'
+          )
+        );
+        console.log(
+          'index for [[6]]',
+          Object.values(pairs).findIndex(
+            (pair) => pair.packet1 === '[[6]]' || pair.packet1 === '[[6]]'
+          )
+        );
+        console.timeEnd('omg');
+        throw new Error('hurray!');
+      }
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next));
+      }
+    }
+  };
+
+  permute(inputArr);
+
+  return result;
+};
+
+const part2 = (input) => {
+  const inputWithDividers = input + `\n\n[[2]]\n[[6]]`;
+  const inputWithoutEmptyLines = inputWithDividers.split('\n\n').join('\n');
+  const rows = inputWithoutEmptyLines.split('\n');
+  permutator(rows);
+};
+
+const checkIfAllPairsAreValid = (pairsArray) => {
+  const validPairs = [];
+  const invalidPairs = [];
+  pairsArray.forEach((pair, index) => {
+    if (pairsArray[index + 1] === undefined) {
+      return true;
+    }
+    const valid = comparePairs(
+      { packet1: pair, packet2: pairsArray[index + 1] },
+      index
+    );
+    if (valid) {
+      validPairs.push(index + 1);
+    } else {
+      invalidPairs.push(index + 1);
+    }
+  });
+  if (invalidPairs.length > 0) return false;
+  return true;
+};
+
+part2(input);
--- 2022/14.js
+++ 2022/14.js
@@ -1,4 +1,196 @@
 const fs = require('fs');
 
-const input = fs.readFileSync('./2022/14_input.txt', 'utf-8');
-const rows = input.split('\n');
+const input = fs.readFileSync('./2022/14_input.txt', 'utf-8').trim();
+
+const testInput = `
+498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
+`.trim();
+
+const partOne = (input) => {
+  let largestX = 0;
+  let largestY = 0;
+  let smallestX = Infinity;
+  let smallestY = Infinity;
+
+  input.split('\n').forEach((row) =>
+    row.split(' -> ').forEach((coordinate) => {
+      const [x, y] = coordinate.split(',');
+      if (x > largestX) largestX = x;
+      if (x < smallestX) smallestX = x;
+      if (y > largestY) largestY = y;
+      if (y < smallestY) smallestY = y;
+    })
+  );
+
+  const Xoffset = largestX - smallestX;
+  const sandStartingPoint = `${500 - smallestX},${0}`;
+
+  const map = [];
+  for (let i = 0; i < +largestY + 1; i++) {
+    map[i] = [];
+  }
+
+  map.forEach((row, rowIndex) => {
+    for (let i = 0; i < +Xoffset + 1; i++) {
+      map[rowIndex][i] = '.';
+    }
+  });
+
+  const markAsRock = (x, y) => {
+    map[y][+x - smallestX] = '#';
+  };
+
+  const markAsSand = (x, y) => {
+    try {
+      console.log(`marking ${x},${y} as sand`);
+      map[y][x] = 'o';
+      console.log(map.map((row) => row.join('')).join('\n'));
+    } catch (error) {
+      console.log(error, 'in markAsSand');
+    }
+  };
+
+  const rockFormations = input.split('\n');
+  rockFormations.forEach((formation) => {
+    const points = formation.split(' -> ');
+    for (let i = 0; i < points.length; i++) {
+      const point = points[i];
+      const [x, y] = point.split(',');
+      if (i === 0) {
+        markAsRock(x, y);
+        continue;
+      }
+      const [prevX, prevY] = points[i - 1]?.split(',');
+      if (x < prevX) {
+        // left
+        const difference = prevX - x;
+        for (let i = 1; i < difference + 1; i++) {
+          const newPoint = `${+prevX - i},${prevY}`;
+          const [newX, newY] = newPoint.split(',');
+          markAsRock(newX, newY);
+        }
+      } else if (x > prevX) {
+        // right
+        const difference = x - prevX;
+        for (let i = 1; i < difference + 1; i++) {
+          const newPoint = `${+prevX + i},${prevY}`;
+          const [newX, newY] = newPoint.split(',');
+          markAsRock(newX, newY);
+        }
+      } else if (y < prevY) {
+        // up
+        const difference = prevY - y;
+        for (let i = 1; i < difference + 1; i++) {
+          const newPoint = `${prevX},${+prevY - i}`;
+          const [newX, newY] = newPoint.split(',');
+          markAsRock(newX, newY);
+        }
+      } else {
+        // down
+        const difference = y - prevY;
+        for (let i = 1; i < difference + 1; i++) {
+          const newPoint = `${prevX},${+prevY + i}`;
+          const [newX, newY] = newPoint.split(',');
+          markAsRock(newX, newY);
+        }
+      }
+    }
+  });
+
+  let sandError = false;
+  let totalSand = 0;
+  let sandLocation = sandStartingPoint;
+
+  const moveSand = (sandLocation) => {
+    console.log(`moving sand from ${sandLocation}`);
+    try {
+      const [sandX, sandY] = sandLocation.split(',');
+      // console.log('sandX', sandX, 'sandY', sandY);
+      // position directly below is open
+      if (map[+sandY + 1][sandX] === '.') {
+        // console.log('below is open:');
+        // console.log(map[+sandY + 1][sandX]);
+        return `${sandX},${+sandY + 1}`;
+      }
+      // position directly below is blocked, check diagonal left
+      let blockedLeft = false;
+      let l = 1;
+      while (blockedLeft === false) {
+        if (map[+sandY + l][+sandX - l] === undefined) {
+          console.log(totalSand, 'totalSand');
+          throw new Error('is this the end?');
+        }
+        console.log('checking down left', map[+sandY + l][+sandX - l]);
+        if (map[+sandY + l][+sandX - l] === '.') {
+          l++;
+        } else {
+          if (l > 1) {
+            blockedLeft = true;
+            console.log();
+            return `${+sandX - (l - 1)},${+sandY + (l - 1)}`;
+          } else {
+            // can't go diagonal left at all, same check for right
+            blockedLeft = true;
+          }
+        }
+      }
+      // position directly below is blocked, check diagonal right
+      let blockedRight = false;
+      let r = 1;
+      while (blockedRight === false) {
+        if (map[+sandY + r][+sandX + r] === undefined) {
+          console.log(totalSand, 'totalSand');
+          throw new Error('is this the end?');
+        }
+        if (map[+sandY + r][+sandX + r] === '.') {
+          r++;
+        } else {
+          if (r > 1) {
+            blockedRight = true;
+            return `${+sandX + (r - 1)},${+sandY + (r - 1)}`;
+          } else {
+            // can't go diagonal right at all, stop here!
+            blockedRight = true;
+            return sandLocation;
+          }
+        }
+      }
+
+      // position can't change
+      // console.log(`cant move from ${sandLocation}`);
+      return sandLocation;
+    } catch (error) {
+      // console.log('sandError!!!', error);
+      // console.log(totalSand, 'should I minus 1?');
+      sandError = true;
+    }
+  };
+
+  while (!sandError) {
+    const newSandLocation = moveSand(sandLocation);
+    console.log('newSandLocation', newSandLocation);
+    if (newSandLocation === sandLocation) {
+      console.log('newSandLocation === sandLocation');
+      const [sandX, sandY] = newSandLocation.split(',');
+      markAsSand(+sandX, +sandY);
+      sandLocation = sandStartingPoint;
+      totalSand++;
+      if (newSandLocation === sandStartingPoint) {
+        console.log(totalSand);
+        throw new Error('sand has reached the top, why does it not fall');
+      }
+    } else {
+      sandLocation = newSandLocation;
+    }
+  }
+
+  return map;
+};
+
+const testMap = partOne(testInput);
+
+fs.writeFileSync(
+  './2022/14_testMap.txt',
+  testMap.map((row) => row.join('')).join('\n')
+);
